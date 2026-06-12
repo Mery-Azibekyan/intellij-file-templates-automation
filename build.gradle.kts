1e634b9bf35dc69dee0157592b5e7615a0bbae2b
Mery Azibekyan <meryazibekyan@gmail.com>
2026-06-13 03:16:32 +0400

    Update Allure report with step annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 
 repositories {
     mavenCentral()
-
     intellijPlatform {
         defaultRepositories()
     }
@@ -20,10 +19,17 @@ dependencies {
         intellijIdeaUltimate("2024.3")
         testFramework(TestFrameworkType.Starter)
     }
-
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.kodein.di:kodein-di-jvm:7.20.2")
     testImplementation("io.qameta.allure:allure-junit5:2.27.0")
+    testImplementation("org.aspectj:aspectjweaver:1.9.20")
+}
+
+allure {
+    adapter {
+        autoconfigure.set(true)
+        aspectjWeaver.set(true)
+    }
 }
 
 kotlin {
